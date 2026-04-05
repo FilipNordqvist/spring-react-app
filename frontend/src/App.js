03bb2f75adbf1ab7fb286e6d9741814fd727c4da
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2026-04-05 18:18:04 +0200

    App working, now time to build something fun!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
+import {useEffect, useState} from 'react'
 
 
 function App() {
+  const [title, setTitle] = useState('Default value');
+
+useEffect(()=> {
+  fetch("http://localhost:8080/home").then(response => response.text())
+  .then(text=>setTitle(text))
+  .catch(error=>console.log("Error fetching", error))
+},[])
+
+
   return (
     <div>
-
-      <h1>React frontend</h1>
+      <h1>React + {title} </h1>
     </div>
-      
   );
 }
 
